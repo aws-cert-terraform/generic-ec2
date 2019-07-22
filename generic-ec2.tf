27cfdb27c90c56f61f9cac33d7743d31ba067f8b
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-22 13:26:20 -0400

    brackets

--- generic-ec2.tf
+++ generic-ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "worker" {
   instance_type        = "${var.instance_type}"
   key_name             = "${var.key_name}"  # to use a key_pair already registered in AWS
   iam_instance_profile = "${var.iam_profile_name}"
-  security_groups      = "[${var.security_groups}]"
+  security_groups      = ["${var.security_groups}"]
   user_data            = "${data.template_file.init.rendered}"
 
   # DNS uses Amazon's internal DNS server.  There is no need to create
